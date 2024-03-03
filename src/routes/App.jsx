edbed14039f203aca85ce06caf1e75b6df6ee0e4
venkimoon4 <venkimoon4@gmail.com>
2024-03-03 19:15:20 +0530

    New_Version-16

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,9 +1,9 @@
-
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Footer from '../components/Footer.jsx'
 import Header from '../components/Header.jsx'
 import { Outlet } from 'react-router-dom'
+import "../components/mediaQueries.css"
 
 
 function App() {
